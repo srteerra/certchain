3c1359e03dfbb2269c09b64b815eb924be5916f4
Guapura89 <betocrack1228@gmail.com>
2023-02-13 21:16:06 -0700

    CER-17 Initial structure :sparkles:

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -8,6 +8,7 @@ const App = () => {
             <Layout>
                 <Routes>
                     <Route path="/" element={<Home />} />
+                    <Route path="*" element={<h1>404: Not Found</h1>} />
                 </Routes>
             </Layout>
         </BrowserRouter>
