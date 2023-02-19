855ef3633c6909259ab54d6db8e5157ed0097da6
Angel Lopez <srterradev@gmail.com>
2023-02-18 23:34:41 -0700

    CER-47 infura-ipfs set up completed

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "../containers/Layout.jsx";
 import Home from "../pages/Home.jsx";
 import Dashboard from "../pages/Dashboard";
+import UploadFile from "../pages/UploadFile.jsx";
 
 const App = () => {
     return (
@@ -10,6 +11,7 @@ const App = () => {
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/dashboard" element={<Dashboard />} />
+                    <Route path="/upload" element={<UploadFile />} />
                     <Route path="*" element={<h1>404: Not Found</h1>} />
                 </Routes>
             </Layout>
