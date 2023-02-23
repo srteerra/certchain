dc9bae8274fded27cd8d688240e02ff0487a5369
Guapura89 <betocrack1228@gmail.com>
2023-02-22 19:06:09 -0700

    add aos animations :package:

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom/client';
 import App from './routes/App.jsx';
 import './index.css';
 
+// Animations
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+// Icons
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
 
 
 library.add(fas, far)
+
+// Initialize AOS
+AOS.init();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
