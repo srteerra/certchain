ba14f9053dde83baef8053c5472c0983143d3bab
Guapura89 <betocrack1228@gmail.com>
2023-03-11 10:02:00 -0700

    feat: change animation and icons dependencies :package:

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,28 +1,41 @@
 import logo from '.././assets/logos/side-nav-logo.png';
-const menu = 'https://cdn-icons-png.flaticon.com/512/4254/4254068.png';
 import { NavLink } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { AttentionSeeker, Fade } from 'react-awesome-reveal';
+import { 
+    HomeIcon,
+    FolderOpenIcon,
+    BuildingLibraryIcon, 
+    PaperAirplaneIcon, 
+    Cog6ToothIcon, 
+    ArrowLeftOnRectangleIcon,
+    Bars3Icon 
+} from '@heroicons/react/24/outline';
+
 
 const SideNav = () => {
     // Active class for nav links
-        const linkClass = ({ isActive }) => (isActive ? "opacity-100 translate-y-6" : 'opacity-50');
+        const linkClass = ({ isActive }) => (isActive ? "opacity-100" : 'opacity-50');
     return (
         <nav className='flex sm:flex-col max-[540px]:justify-center justify-between text-white max-[540px]:w-full w-10/12 sm:w-full py-6 sm:py-14 text-2xl'>
             <div>
-                <ul className='flex items-center sm:flex-col gap-10 '>
-                    <li><NavLink to="/"><img src={ logo } alt="logo" className='mx-auto min-w-[40px]' /></NavLink></li>
-                    <li><NavLink className={linkClass} to="/"><FontAwesomeIcon icon="fa-solid fa-house" /></NavLink></li>
-                    <li><NavLink className={linkClass} to="/documents"><FontAwesomeIcon icon="fa-solid fa-folder-open" /></NavLink></li>
-                    <li><NavLink className={linkClass} to="/d"><FontAwesomeIcon icon="fa-solid fa-building-columns" /></NavLink></li>
-                    <FontAwesomeIcon icon="fa-solid fa-bars" className="min-[540px]:hidden" />
-                </ul>
+                <Fade duration={500} direction='left' cascade>
+                    <ul className='flex items-center sm:flex-col gap-10 '>
+                        <li><NavLink to="/"><img src={ logo } alt="logo" className='mx-auto min-w-[40px]' /></NavLink></li>
+                        <li><NavLink className={linkClass} to="/"><HomeIcon className='h-8 w-8' /></NavLink></li>
+                        <li><NavLink className={linkClass} to="/documents"><FolderOpenIcon className='h-8 w-8'/></NavLink></li>
+                        <li><NavLink className={linkClass} to="/d"><BuildingLibraryIcon className='h-8 w-8'/></NavLink></li>
+                        <Bars3Icon className='h-8 w-8 min-[540px]:hidden' />
+                    </ul>
+                </Fade>
             </div>
             <div className='flex items-center sm:justify-center'>
-                <ul className='max-[540px]:hidden flex sm:flex-col gap-10'>
-                    <li><NavLink className={linkClass} to="/l"><FontAwesomeIcon icon="fa-solid fa-paper-plane" /></NavLink></li>
-                    <li><NavLink className={linkClass} to="/b"><FontAwesomeIcon icon="fa-solid fa-gear"  /></NavLink></li>
-                    <li><NavLink to="#"><FontAwesomeIcon icon="fa-solid fa-arrow-right-from-bracket" /></NavLink></li>
-                </ul>
+                <Fade direction='left' cascade>
+                    <ul className='max-[540px]:hidden flex sm:flex-col gap-10'>
+                        <li><NavLink className={linkClass} to="/l"><PaperAirplaneIcon className='h-8 w-8'/></NavLink></li>
+                        <li><NavLink className={linkClass} to="/b"><Cog6ToothIcon className='h-8 w-8'/></NavLink></li>
+                        <li><NavLink to="#"><ArrowLeftOnRectangleIcon className='h-8 w-8'/></NavLink></li>
+                    </ul>
+                </Fade>
             </div>
         </nav>
     );
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './routes/App.jsx';
 import './index.css';
 
-// Animations
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-
 // Icons
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
@@ -15,8 +11,6 @@ import { far } from '@fortawesome/free-regular-svg-icons';
 
 library.add(fas, far)
 
-// Initialize AOS
-AOS.init();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
